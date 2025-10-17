c9236859fe9e330dae2346d5dca5e02b88f15a02
leonardo.ataides <leonardoataidesjesus@gmail.com>
2025-10-17 19:40:54 -0300

    implementando interface em java

---
+++ AULA-17-10/src/Passaro.java
@@ -0,0 +1,12 @@
+public class Passaro implements Voavel {
+    private String nome;
+
+    public Passaro(String nome){
+        this.nome = nome;
+    }
+
+    @Override
+    public void voar() {
+        System.out.println(nome + " está voando " + ALTITUDE_MAXIMA + " metros acima do solo.");
+    }
+}
