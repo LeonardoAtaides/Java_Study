36de3c8e04a51ae96217f1bb348e6720174bd719
leonardo.ataides <leonardoataidesjesus@gmail.com>
2025-10-17 20:20:08 -0300

    Criado base para a ATV

---
+++ AULA-17-10/ATV-CARRO-ELETRICO/src/Main.java
@@ -0,0 +1,22 @@
+/*
+Enunciado: Desafio Veiulos Eletrico
+
+crie duas interfaces
+1- recarregável{
+    -metodo abstrato *regarregar*, que recebe parametros (int, tempo minutos),imprima "algo como regarregando por X minutos"
+            - constante int TEMPO_MAXIMO = 30
+}
+
+2- Autonomo {
+    - metodo abstrato *dirigirautonomo* , que recebe como paremetro como (string, destino), imprimindo "dirigindo autononamente para o destino"
+    constante boolean SUPORTE_GPS = TRUE
+}
+
+Crie uma classe carro eletrico que implemente ambas as interfaces, adicione o atributo string MODELO no construtor, nas implementações use o modelo - Ex: "O modelo X está regarregando"
+Na classe teste veiculo- (MAIN) crie um objeto CarroEltrico, chame o metodo via referencias das interfaces (polimorfismo) e use as constantes*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("oi");
+        }
+    }
