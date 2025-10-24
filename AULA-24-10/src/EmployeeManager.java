169f9820b7cc808a67ef0f2321cf6c702c604f76
leonardo.ataides <leonardoataidesjesus@gmail.com>
2025-10-24 19:47:28 -0300

    update employmanager

---
+++ AULA-24-10/src/EmployeeManager.java
@@ -0,0 +1,15 @@
+public class EmployeeManager {
+    public void  saveToDatabase(Employee e){
+
+    }
+
+    public void  calcularSalary(Employee e){
+
+    }
+
+
+    public void  senEmail(Employee e){
+
+    }
+
+}
